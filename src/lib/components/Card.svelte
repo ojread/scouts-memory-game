583e0c56a29f7e7c3f6ef6c2836658f5b8771188
Oliver Read <oliver@reads.org.uk>
2026-03-29 21:59:53 +0100

    Remove classes

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -12,12 +12,12 @@
     class:rotate-y-180={flipped}
   >
     <div
-      class="front absolute inset-0 size-full overflow-hidden rounded-2xl border-4 backface-hidden"
+      class="absolute inset-0 size-full overflow-hidden rounded-2xl border-4 backface-hidden"
     >
       <img src={back} alt="Card back" />
     </div>
     <div
-      class="front absolute inset-0 size-full rotate-y-180 overflow-hidden rounded-2xl border-4 bg-gray-300 backface-hidden sm:p-4"
+      class="absolute inset-0 size-full rotate-y-180 overflow-hidden rounded-2xl border-4 bg-gray-300 backface-hidden sm:p-4"
     >
       <img src={front} alt="Card front" />
     </div>
