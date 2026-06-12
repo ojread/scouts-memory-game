7807c672dbe96d8e42d213e63363fd5afd885940
Oliver Read <oliver@reads.org.uk>
2026-06-12 06:06:57 +0100

    Fisher-Yates Shuffle

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,19 @@
-// Shuffle an array of anything into a random order.
+// Fisher-Yates Shuffle algorithm
 export function shuffle<T>(array: T[]): T[] {
-    return array.sort(() => Math.random() - 0.5);
+    // export function shuffle(array) {
+    // set the index to the arrays length
+    let i = array.length, j, temp;
+    // create a loop that subtracts everytime it iterates through
+    while (--i > 0) {
+        //  create a random number and store it in a variable
+        j = Math.floor(Math.random() * (i + 1));
+        // create a temporary position from the item of the random number    
+        temp = array[j];
+        // swap the temp with the position of the last item in the array    
+        array[j] = array[i];
+        // swap the last item with the position of the random number 
+        array[i] = temp;
+    }
+    // return the array when it completes
+    return array;
 }
